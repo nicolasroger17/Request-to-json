8ba040c279bc68510107028e8061dbe76b926b60
nicolasroger17 <nicolasroger17@gmail.com>
2014-12-03 18:22:11 -0500

    upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+	<title>Request to Json</title>
+	<link rel="stylesheet" type="text/css" href="/RequestToHeader/css/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/RequestToHeader/css/style.css">
+</head>
+<body>
+	<div class="jumbotron">
+      <div class="container">
+        <h1>Request to Json</h1>
+        <p>Paste your request in the first box</p>
+      </div>
+    </div>
+    <div class="container">
+    	<div class="form-group f1">
+    		<h3>Request:</h3>
+    		<textarea id="req" class="form-control" rows="20"></textarea>
+    	</div>
+    	<div class="form-group f2">
+    		<h3>Params:</h3>
+    		<div class='paramType'>
+	    		<label>Get: <input type='radio' name='param' value='get' checked></label>
+	    		<label>Post: <input type='radio' name='param' value='post'></label>
+	    	</div>
+    		<textarea id="param" class="form-control" rows="18"></textarea>
+    	</div>
+    	<div style='clear:both'></div>
+    	<button id="transform" title="Copy header" data-clipboard-target='head' class="btn btn-primary">Transform</button>
+    
+    	<div class="form-group f3">
+    		<h3>Json:</h3>
+    		<textarea id="head" class="form-control" rows="20"></textarea>
+    	</div>
+    </div>
+    <script type="text/javascript" src="/RequestToHeader/js/jquery.js"></script>
+	<script type="text/javascript" src="/RequestToHeader/js/zeroclipboard/dist/ZeroClipboard.min.js"></script>
+	<script type="text/javascript" src="/RequestToHeader/js/js.js"></script>
+</body>
+</html>
---
+++ js/js.js
@@ -0,0 +1,40 @@
+function parseRequest(){
+	var header = "headers = {\n";
+	var lines = $("#req").val().split('\n');
+
+	for(var i = 1; i < lines.length; i++){
+		var key = lines[i].substring(0, lines[i].indexOf(":"));
+		console.log(key);
+		if(key != "Connection" && key != "Host" && key != "Cookie")
+			header += "\t'" + key + "': '" + lines[i].substring(key.length + 2, lines[i].length) + "',\n";
+	}
+
+	var secondInput = $("#param").val();
+	console.log(secondInput);
+	if(secondInput != ""){
+		if($("input[name='param']:checked").val() == "get")
+			header += "\t'params': '" + secondInput + "',\n";
+		else
+			header += "\t'payload': '" + secondInput + "',\n";
+	}
+	
+	header = header.slice(0, header.length - 2) + "\n}";
+	$("#head").val(header);
+	console.log(header);
+}
+
+$(document).ready(function() {
+	var clip = new ZeroClipboard($("#transform"));
+
+	clip.on("ready", function() {
+
+	    this.on("beforecopy", function(event) {
+	    	parseRequest();
+	    });
+
+	  clip.on("error", function(event) {
+	    ZeroClipboard.destroy();
+	  });
+
+	});
+});
